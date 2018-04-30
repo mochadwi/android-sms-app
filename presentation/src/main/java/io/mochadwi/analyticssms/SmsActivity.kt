cbb8935007f2cadee1638c2caf784175d05aea3e
Mochamad Iqbal Dwi Cahyo <mochadwi20@gmail.com>
2018-04-30 13:35:12 +0700

    [ADD] Separation of concern using clean architecture - domain (java library) - data (network, services, etc) - presentation (ui related, mvp)

--- presentation/src/main/java/io/mochadwi/analyticssms/SmsActivity.kt
+++ presentation/src/main/java/io/mochadwi/analyticssms/SmsActivity.kt
@@ -3,10 +3,12 @@ package io.mochadwi.analyticssms
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SmsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
